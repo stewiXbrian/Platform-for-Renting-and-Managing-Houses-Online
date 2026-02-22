a4582fca2461a57f3757092471c8043b1e947e52
ayoub <115874830+stewiXbrian@users.noreply.github.com>
2026-02-22 04:43:25 +0100

    Change geoApi key and clean up comments
    Updated API key for geoApi and removed commented code.

--- src/pages/geoApi.js
+++ src/pages/geoApi.js
@@ -1,30 +1,8 @@
 
-/**
- * in the calling file ,i have 2 ways to get data:
- * methode1:
- * 
- * updateMarker("sousse")
-    .then(coords => {
-      console.log("Fetched coordinates:", coords);
-    })
-    .catch(error => {
-      console.error("Error updating marker:", error);
-    });
-    
-    methode2:
 
-    (async () => {
-    try {
-      const coords = await updateMarker("sousse");
-      console.log("Fetched coordinates:", coords);
-    } catch (error) {
-      console.error("Error updating marker:", error);
-    }
-  })();
- */
  
   export default async function updateMarker(city) {
-    const apiKey = 'ab451f89ef9f4fefa22e3f9e39086606';
+    const apiKey = 'geoapi key';
     const country = 'Tunisia';
     const query = `${city}, ${country}`;
   
